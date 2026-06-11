14e897cea20dbeabcbbb2526d82d7e6b00dd1a0f
Said Arifullin <saidarifullin@Saids-MacBook-Air.local>
2026-06-11 12:33:29 +0300

    refactor: monorepo with EduRequestSystemAPI and TCA.Desktop

---
+++ EduRequestSystemAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using EduRequestSystemAPI.Interfaces;
+using EduRequestSystemAPI.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduRequestSystemAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("RegUser")]
+        public async Task<IActionResult> RegUser([FromBody] RegUserModel newUser)
+        {
+            return await _userService.RegUserAsync(newUser);
+        }
+
+        [HttpPost]
+        [Route("AuthUser")]
+        public async Task<IActionResult> AuthUser([FromBody] AuthUserModel loginData)
+        {
+            return await _userService.AuthUserAsync(loginData);
+        }
+    }
+}
---
+++ EduRequestSystemAPI/Requests/CreateRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace EduRequestSystemAPI.Requests
+{
+    public class CreateRequest
+    {
+        public string Topic { get; set; }
+        public string Description { get; set; }
+        public string? ContactInfo { get; set; }
+        public int DirectionId { get; set; }
+        public int TrainingFormatId { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
