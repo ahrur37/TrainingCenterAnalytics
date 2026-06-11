fb47332d1e9b21d647066566e65f74836378812f
Said Arifullin <saidarifullin@Saids-MacBook-Air.local>
2026-06-11 19:02:44 +0300

    add: auth and reg views

---
+++ Desktop/Views/RegistrationView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TCA.Desktop.Views;
+
+public partial class RegistrationView : UserControl
+{
+    public RegistrationView()
+    {
+        InitializeComponent();
+    }
+}
