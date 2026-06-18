841d0a328289461ebf96789ea178d9ecb7793285
Said Arifullin <saidarifullin@Saids-MacBook-Air.local>
2026-06-18 19:42:06 +0300

    add(dictionaries)/add uc with manuals

---
+++ Desktop/Views/DictionariesView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TCA.Desktop.Views;
+
+public partial class DictionariesView : UserControl
+{
+    public DictionariesView()
+    {
+        InitializeComponent();
+    }
+}
